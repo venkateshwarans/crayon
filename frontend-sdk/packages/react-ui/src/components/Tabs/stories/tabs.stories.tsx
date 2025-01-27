cdb99f34c7255a155d3b8e6ec16751aac15a3175
i-subham <i-subham@thesys.dev>
2025-01-27 14:10:54 +0530

    Refactor Code

--- frontend-sdk/packages/react-ui/src/components/Tabs/stories/tabs.stories.tsx
+++ frontend-sdk/packages/react-ui/src/components/Tabs/stories/tabs.stories.tsx
@@ -1,4 +1,5 @@
 import type { Meta, StoryObj } from "@storybook/react";
+import { Settings, ShieldCheck, UserRound } from "lucide-react";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "../Tabs";
 import "../tabs.scss";
 
@@ -22,9 +23,9 @@ export const Default: Story = {
   render: () => (
     <Tabs defaultValue="tab1">
       <TabsList>
-        <TabsTrigger value="tab1">Account</TabsTrigger>
-        <TabsTrigger value="tab2">Password</TabsTrigger>
-        <TabsTrigger value="tab3">Settings</TabsTrigger>
+        <TabsTrigger value="tab1" text="Account" />
+        <TabsTrigger value="tab2" text="Password" />
+        <TabsTrigger value="tab3" text="Settings" />
       </TabsList>
       <TabsContent value="tab1">
         <h3>Account Settings</h3>
@@ -46,11 +47,9 @@ export const Disabled: Story = {
   render: () => (
     <Tabs defaultValue="tab1">
       <TabsList>
-        <TabsTrigger value="tab1">Active Tab</TabsTrigger>
-        <TabsTrigger value="tab2" disabled>
-          Disabled Tab
-        </TabsTrigger>
-        <TabsTrigger value="tab3">Another Tab</TabsTrigger>
+        <TabsTrigger value="tab1" text="Active Tab" />
+        <TabsTrigger value="tab2" disabled text="Disabled Tab" />
+        <TabsTrigger value="tab3" text="Another Tab" />
       </TabsList>
       <TabsContent value="tab1">
         <p>This is the content for the active tab.</p>
@@ -69,12 +68,8 @@ export const CustomStyling: Story = {
   render: () => (
     <Tabs defaultValue="tab1" className="custom-tabs">
       <TabsList className="custom-tabs-list">
-        <TabsTrigger value="tab1" className="custom-trigger">
-          Tab 1
-        </TabsTrigger>
-        <TabsTrigger value="tab2" className="custom-trigger">
-          Tab 2
-        </TabsTrigger>
+        <TabsTrigger value="tab1" text="Tab 1" />
+        <TabsTrigger value="tab2" text="Tab 2" />
       </TabsList>
       <TabsContent value="tab1" className="custom-content">
         <p>Content with custom styling.</p>
@@ -86,13 +81,37 @@ export const CustomStyling: Story = {
   ),
 };
 
+export const WithIcon: Story = {
+  render: () => (
+    <Tabs defaultValue="tab1" variant="clear">
+      <TabsList>
+        <TabsTrigger value="tab1" text="Account" icon={<UserRound />} />
+        <TabsTrigger value="tab2" text="Password" icon={<ShieldCheck />} />
+        <TabsTrigger value="tab3" text="Settings" icon={<Settings />} />
+      </TabsList>
+      <TabsContent value="tab1">
+        <h3>Account Settings</h3>
+        <p>Manage your account preferences here.</p>
+      </TabsContent>
+      <TabsContent value="tab2">
+        <h3>Password Settings</h3>
+        <p>Change your password and security settings.</p>
+      </TabsContent>
+      <TabsContent value="tab3">
+        <h3>General Settings</h3>
+        <p>Configure your application settings.</p>
+      </TabsContent>
+    </Tabs>
+  ),
+};
+
 export const Clear: Story = {
   render: () => (
     <Tabs defaultValue="tab1" variant="clear">
       <TabsList>
-        <TabsTrigger value="tab1">Account</TabsTrigger>
-        <TabsTrigger value="tab2">Password</TabsTrigger>
-        <TabsTrigger value="tab3">Settings</TabsTrigger>
+        <TabsTrigger value="tab1" text="Account" />
+        <TabsTrigger value="tab2" text="Password" />
+        <TabsTrigger value="tab3" text="Settings" />
       </TabsList>
       <TabsContent value="tab1">
         <h3>Account Settings</h3>
@@ -114,9 +133,9 @@ export const Card: Story = {
   render: () => (
     <Tabs defaultValue="tab2" variant="card">
       <TabsList>
-        <TabsTrigger value="tab1">Account</TabsTrigger>
-        <TabsTrigger value="tab2">Password</TabsTrigger>
-        <TabsTrigger value="tab3">Settings</TabsTrigger>
+        <TabsTrigger value="tab1" text="Account" />
+        <TabsTrigger value="tab2" text="Password" />
+        <TabsTrigger value="tab3" text="Settings" />
       </TabsList>
       <TabsContent value="tab1">
         <h3>Account Settings</h3>
@@ -138,9 +157,9 @@ export const Sunk: Story = {
   render: () => (
     <Tabs defaultValue="tab3" variant="sunk">
       <TabsList>
-        <TabsTrigger value="tab1">Account</TabsTrigger>
-        <TabsTrigger value="tab2">Password</TabsTrigger>
-        <TabsTrigger value="tab3">Settings</TabsTrigger>
+        <TabsTrigger value="tab1" text="Account" />
+        <TabsTrigger value="tab2" text="Password" />
+        <TabsTrigger value="tab3" text="Settings" />
       </TabsList>
       <TabsContent value="tab1">
         <h3>Account Settings</h3>
