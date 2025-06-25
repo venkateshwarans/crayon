e5ec4ffa9db87b3c81c510a146735b9b4ace76a5
Sivasankar Naidu Seelam <sivasankarnaidu.seelam@Sivasankars-MacBook-Pro.local>
2025-06-25 07:01:44 +0530

    Combo Chart, Candlestick Chart, Histogram Chart, Wordtree Chart

---
+++ js/packages/react-ui/src/components/Charts/WordTreeChart/stories/wordTreeChart.stories.tsx
@@ -0,0 +1,58 @@
+import { Meta, StoryObj } from "@storybook/react";
+import { WordTree, WordTreeProps } from "../WordTreeChart";
+
+const meta: Meta<WordTreeProps> = {
+  title: "Components/Charts/WordTree",
+  component: WordTree,
+  parameters: {
+    layout: "centered",
+    docs: {
+      description: `
+        \`\`\`tsx
+        import { WordTree } from '@crayon-ui/react-ui/Charts/WordTree';
+        \`\`\`
+      `,
+    },
+  },
+  tags: ["!dev", "autodocs"],
+};
+
+export default meta;
+
+const implicitData = [
+  ["Phrases"],
+  ["cats are better than dogs"],
+  ["cats eat kibble"],
+  ["cats are better than hamsters"],
+  ["cats are awesome"],
+];
+
+const explicitData = [
+  ["id", "childLabel", "parent", "size", { role: "style" }],
+  [0, "Life", -1, 1, "black"],
+  [1, "Animals", 0, 1, "black"],
+  [2, "Mammals", 1, 1, "black"],
+  [3, "Cats", 2, 1, "black"],
+  [4, "Dogs", 2, 1, "black"],
+];
+
+export const ImplicitWordTreeStory: StoryObj<WordTreeProps> = {
+  name: "Implicit Word Tree",
+  args: {
+    data: implicitData,
+    width: "100%",
+    height: "100%",
+    format: "implicit",
+    word: "cats",
+  },
+};
+
+export const ExplicitWordTreeStory: StoryObj<WordTreeProps> = {
+  name: "Explicit Word Tree",
+  args: {
+    data: explicitData,
+    width: "100%",
+    height: "100%",
+    format: "explicit",
+  },
+};
