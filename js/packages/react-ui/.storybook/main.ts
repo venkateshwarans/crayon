47aced3e13729fd7d1f1d9c427303c50ce86f2c2
Venkateshwaran Selvaraj <venkateshwaran.s@sarasanalytics.com>
2025-09-06 12:59:35 +0530

    Merging with Upstream #2

--- js/packages/react-ui/.storybook/main.ts
+++ js/packages/react-ui/.storybook/main.ts
@@ -33,7 +33,7 @@ const config: StorybookConfig = {
       server: {
         allowedHosts: [".trycloudflare.com", "127.0.0.1", "localhost"],
       },
-      base: "/ui/",
+      base: "/",
       resolve: {
         alias: {
           "@crayonai/react-core": path.resolve(__dirname, "../../react-core/src/index.ts"),
